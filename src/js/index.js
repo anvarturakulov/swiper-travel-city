700da6503127983b26a1e37f9936307267ad83d3
Anvar <anvarsam@mail.ru>
2020-11-10 02:44:00 +0500

    add few changs( infobox, mobile adaptive and other)

--- src/js/index.js
+++ src/js/index.js
@@ -51,11 +51,43 @@ let swiper;
     swiper = new Swiper('.swiper-container', {
         slidesPerView: 6,
         slidesPerColumn: 2,
-        //spaceBetween: 30,
+        spaceBetween: 0,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+        breakpoints: {
+            319: {
+              slidesPerView: 1,
+              slidesPerColumn: 2
+            },
+
+            374: {
+              slidesPerView: 2,
+              slidesPerColumn: 2
+            },
+
+            520: {
+              slidesPerView: 3,
+              slidesPerColumn: 2
+            },
+            850: {
+                slidesPerView: 4,
+                slidesPerColumn: 2
+            },
+            1000: {
+                slidesPerView: 5,
+                slidesPerColumn: 2
+            },
+            1300: {
+                slidesPerView: 6,
+                slidesPerColumn: 2
+              }
+          }
       }); 
   }
   
@@ -86,25 +118,73 @@ let swiper;
         showSliderByCategory(e.target.getAttribute('data-category'));
         e.target.classList.add('active-link');
       });
-      
   });
+
+
+  let selectForm = document.querySelector('.select__form form select');
+  
+  selectForm.addEventListener('change', (e) => {
+    showSliderByCategory(e.target.value);
+  });
+
+
 
 
   function hoverInfoBoxPosition () {
     let swiperEl = document.querySelectorAll('.swiper-slide');
-    
     swiperEl.forEach(item =>{
-        item.addEventListener('click', ()=> {
-
-            let itemKorr = item.getBoundingClientRect();
-            let swiperContainer = document.querySelector('.swiper-container');
-
-            console.log(itemKorr);
-            console.log(window.innerWidth);
-            console.log(swiperContainer.getBoundingClientRect());
-            
+        item.addEventListener('mouseenter', ()=> {
+            let infoWindow = item.querySelector('.content');
+            infoWindow.style.width = '400px';
+            if (+window.innerWidth <= 700) {
+                infoWindow.style.width = '325px';
+            } 
+            //infoWindow.style.display = 'block';
+            if (+window.innerWidth >= 500) {
+                let itemSize = item.getBoundingClientRect();
+                let imgSize = item.querySelector('img').getBoundingClientRect();
+                let imgRightSideSize = window.innerWidth - imgSize.right;
+                let imgLeftSideSize = imgSize.right-imgSize.width;
+                //console.log(`Left ${imgLeftSideSize} === Rigth ${imgRightSideSize} == winfow ${window.innerWidth}`);
+                if (imgRightSideSize >= parseInt(infoWindow.style.width)) {
+                    infoWindow.style.left = `${imgSize.width+((itemSize.width-imgSize.width)/2)}px`;
+                } else if (imgLeftSideSize >= parseInt(infoWindow.style.width)) {
+                    infoWindow.style.left = `-${parseInt(infoWindow.style.width)-((itemSize.width-imgSize.width)/2)}px`;
+                }
+            } else{
+                   infoWindow.style.display = 'none';
+                   item.querySelector('img').style.cursor = 'pointer';
+                // let wrapperSize = document.querySelector('.swiper-wrapper').getBoundingClientRect();
+                // let mobileInfoBox = document.querySelector('.mobile__info-box');
+                // mobileInfoBox.querySelector('.img-box').style.cssText = `background-image: url(${item.querySelector('img').src});`;
+                // mobileInfoBox.querySelector('.content-text').textContent = item.querySelector('.content-text').textContent;
+                // mobileInfoBox.style.top = `${wrapperSize.top}px`;
+                // mobileInfoBox.style.display = 'block';
+            }
         });
     });
-  }
 
-  categoryItem[0].classList.add('active-link');
+    swiperEl.forEach(item =>{
+        item.addEventListener('click', ()=> {
+            let infoWindow = item.querySelector('.content');
+            if (+window.innerWidth >= 500) {
+            } else{
+                infoWindow.style.display = 'none';
+                let wrapperSize = document.querySelector('.swiper-wrapper').getBoundingClientRect();
+                let mobileInfoBox = document.querySelector('.mobile__info-box');
+                mobileInfoBox.querySelector('.img-box').style.cssText = `background-image: url(${item.querySelector('img').src});`;
+                mobileInfoBox.querySelector('.content-text').textContent = item.querySelector('.content-text').textContent;
+                mobileInfoBox.style.top = `${wrapperSize.top}px`;
+                mobileInfoBox.style.display = 'block';
+            }
+        });
+    });
+
+}
+
+    let mobileInfoBox = document.querySelector('.mobile__info-box');
+    mobileInfoBox.querySelector('.close').addEventListener('click', () => {
+        mobileInfoBox.style.display = 'none';
+    });
+
+    categoryItem[0].classList.add('active-link');
